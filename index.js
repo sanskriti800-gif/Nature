1c3a70ad94c8bb4bd9d1aed1aae7c80f6b112f4d
sanskriti <gupta.sanskriti08@gmail.com>
2021-07-01 01:39:42 +0530

    commit changes

---
+++ index.js
@@ -0,0 +1,20 @@
+$('.single-item').slick();
+
+
+
+
+
+
+
+
+
+//     $("button").click(function(){
+//         var innerButton = this.innerHTML;
+//         makesomenoise(innerButton);
+    
+//     });
+    
+// function makesomenoise(name){
+//     var audio = new Audio("audio/"+name+".mp3");
+//     audio.play();
+// }
